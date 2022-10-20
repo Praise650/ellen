17ec2db33d3936e61f2403f233e48947ef6d50e2
hp <ayokanmi1275@gmail.com>
2022-10-20 02:26:34 +0100

    changed app name to Bloxity from ellen

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ellen',
+      title: 'Bloxity',
        debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
